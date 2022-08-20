bb8f2b9f6e147c9681a842a116138ec8a6d899f9
Reid Yonkers <81446209+chaiai@users.noreply.github.com>
2022-08-20 15:07:22 -0400

    Update setup.sh

--- nano/setup.sh
+++ nano/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ~
+
 sudo apt update
 sudo apt -y purge libreoffice*
 sudo apt -y autoremove
@@ -10,8 +12,7 @@ sudo -H pip3 install -U pip
 sudo -H pip install -U jetson-stats
 
 git clone https://github.com/dusty-nv/jetson-containers
-
-cd jetson-containers
+git clone https://github.com/NVIDIA-AI-IOT/ros2_torch_trt
 
 neofetch
 
