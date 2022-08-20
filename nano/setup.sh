88482e11e47d7a8152b507691f424f6f056e075d
Reid Yonkers <81446209+chaiai@users.noreply.github.com>
2022-08-20 12:44:02 -0400

    Update setup.sh

--- nano/setup.sh
+++ nano/setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-sudo -e
-
 sudo apt update
 sudo apt -y install neofetch
+sudo apt -y install python3-pip
+
+sudo -H pip3 install -U pip
+sudo -H pip install -U jetson-stats
 
 git clone https://github.com/dusty-nv/jetson-containers
 
 cd jetson-containers
 
 neofetch
+
+echo "Set NVIDIA default runtime!!"
